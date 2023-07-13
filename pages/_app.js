1ac1028d068966c14999d97a03c43d8b355e6622
David <98425402+DavidFBD@users.noreply.github.com>
2023-07-13 15:22:53 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import 'tailwindcss/tailwind.css'
 import '../globals.css';
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 function MyApp({ Component, pageProps }) {
   return (
-    <main className={inter.className}>
+    <main className="font-montserrat">
       <Component {...pageProps} />
     </main>
   )
