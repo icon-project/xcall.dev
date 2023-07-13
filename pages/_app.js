19ec26967d6580dfa8634a8fbb3e599c79e7e08c
David <98425402+DavidFBD@users.noreply.github.com>
2023-07-13 15:14:14 +0200

    Added links

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import 'tailwindcss/tailwind.css'
 import '../globals.css';
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default MyApp
