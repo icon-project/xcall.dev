3b39ef5ba6fa708f0218373c3a38273c5c85709d
David <98425402+DavidFBD@users.noreply.github.com>
2023-08-11 10:36:00 +0300

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -31,19 +31,7 @@ const config: DocsThemeConfig = {
             }
           ],
         },
-        twitter: {
-          handle: '@helloiconworld',
-          site: 'https://xcall.dev',
-          cardType: 'summary_large_image',
-          images: [
-            {
-              url: 'https://xcall.dev/images/link-preview.jpg',
-              width: 1200,
-              height: 630,
-              alt: 'xCall Documentation',
-            }
-          ],
-        },
+        
       } 
     } else {
       return {
@@ -54,14 +42,16 @@ const config: DocsThemeConfig = {
           description: "A cross-chain messaging interface deployable on any protocol",
           type: 'website',
           url: 'https://xcall.dev',
-          
+          images: [
+            {
+              url: 'https://xcall.dev/images/link-preview.jpg',
+              width: 1200,
+              height: 630,
+              alt: 'xCall Documentation',
+            }
+          ],
         },
-        twitter: {
-          handle: '@helloiconworld',
-          site: 'https://xcall.dev',
-          cardType: 'summary_large_image',
-          
-        },
+        
       }
     }
     
@@ -121,7 +111,10 @@ const config: DocsThemeConfig = {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Language" content="en" />
         <meta name="apple-mobile-web-app-title" content="xCall" />
-        <meta name="twitter:image" content="https://xcall.dev/images/link-preview.jpg"></meta>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/images/link-preview.jpg" />
+        <meta name="twitter:site:domain" content="xcall.dev" />
+        <meta name="twitter:url" content="https://xcall.dev" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
