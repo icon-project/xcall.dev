a7ccf018139e39f50429e31fc88f5ed6b7138bcb
David <98425402+DavidFBD@users.noreply.github.com>
2023-08-14 18:42:26 +0300

    Consent banner addition

---
+++ components/consent.js
@@ -0,0 +1,49 @@
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react';
+import { setCookie, hasCookie } from 'cookies-next';
+
+export default function Consent(){
+    const [consent, setConsent] = useState(true);
+    useEffect(() => {
+      setConsent(hasCookie('localConsent'));
+    }, []);
+  
+    const acceptCookie = () => {
+      setConsent(true);
+      setCookie('localConsent', 'true', { maxAge: 60 * 60 * 24 * 365 });
+      gtag('consent', 'update', {
+        ad_storage: 'granted',
+        analytics_storage: 'granted',
+      });
+      console.log('accepting cookies');
+    };
+    const closeP = () => {
+      setConsent(true);
+      console.log('closing');
+    };
+    const denyCookie = () => {
+      setConsent(true);
+      setCookie('localConsent', 'false', { maxAge: 60 * 60 * 24 * 365 });
+      console.log('denying cookie');
+    };
+    if (consent === true) {
+      return null;
+    }
+    
+    return (
+        <div className={`my-10 mx-auto max-w-max md:max-w-screen-sm
+                        fixed bottom-0 left-0 right-0 
+                        flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
+                         bg-gray-700 rounded-lg shadow`}>
+
+            <div className='text-center'>
+                <Link href="/privacy-policy#cookies-and-web-beacons"><p>We use <span className='font-bold text-sky-400'>cookies</span> on our site.</p></Link>
+            </div>
+
+            
+            <div className='flex gap-2'>
+                <button className='px-5 py-2 text-gray-300 border-gray-900 rounded-md' onClick={(e) => denyCookie()}>Decline</button>
+                <button className='px-5 py-2 text-white bg-gray-900 rounded-lg' onClick={() => {acceptCookie()}}>Allow Cookies</button>
+            </div>   
+        </div>
+    )}
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,53 @@
 import 'tailwindcss/tailwind.css'
 import '../globals.css';
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
+import Layout from '../components/layout';
+import { getCookie } from 'cookies-next';
 
 function MyApp({ Component, pageProps }) {
+  const consent = getCookie('localConsent');
   return (
     <main lang="en" className="font-montserrat">
-      <Component {...pageProps} />
+      <Script
+        id="gtag"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            
+            gtag('consent', 'default', {
+              'ad_storage': 'denied',
+              'analytics_storage': 'denied'
+            });
+            
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','GTM-5N6NGB9J');`,
+        }}
+      />
+
+      {consent === true && (
+        <Script
+          id="consupd"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            gtag('consent', 'update', {
+              'ad_storage': 'granted',
+              'analytics_storage': 'granted'
+            });
+          `,
+          }}
+        />
+      )}
+      
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>     
       <Analytics />
     </main>
   )
