2f836b5a5f109606871df70954c4e267547b3027
David <98425402+DavidFBD@users.noreply.github.com>
2024-01-22 14:06:31 +0100

    Updates

--- components/blockchains.js
+++ components/blockchains.js
@@ -5,5 +5,10 @@ export const blockchains = [
     {"name": "Binance Smart Chain", "status": "mainnet", "protocol": ["BTP"], "logo": "/chainlogos/binance-smart-chain.svg"},
     {"name": "Ethereum", "status": "mainnet", "protocol": ["BTP"], "logo": "/chainlogos/ethereum.svg"},
     {"name": "Havah", "status": "testnet", "protocol": ["BTP"], "logo": "/chainlogos/havah.svg"},
+    {"name": "Base", "status": "testnet", "protocol": ["xCall Path"], "logo": "/chainlogos/havah.svg"},
+    {"name": "Avalanche", "status": "testnet", "protocol": ["xCall Path"], "logo": "/chainlogos/havah.svg"},
+    {"name": "Composable", "status": "development", "protocol": ["IBC"], "logo": "/chainlogos/havah.svg"},
+    {"name": "Optimism", "status": "development", "protocol": [""], "logo": "/chainlogos/havah.svg"},
+    {"name": "Arbitrum", "status": "development", "protocol": [""], "logo": "/chainlogos/havah.svg"},
     {"name": "ICON", "status": "mainnet", "protocol": ["BTP", "IBC"], "logo": "/chainlogos/ICON.svg"}
 ];
--- components/chainchecker.js
+++ components/chainchecker.js
@@ -2,64 +2,63 @@ import React, { useState, useEffect } from 'react';
 import { blockchains } from './blockchains';
 
 const ChainChecker = () => {
-  const [selectedChain, setSelectedChain] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
   const [matchingChains, setMatchingChains] = useState([]);
-
-  // Update the matching chains when selectedChain changes
+  const bgopacity = matchingChains.length > 0 ? 20 : 0;
+  
+  // Update the matching chains based on the search term
   useEffect(() => {
-    if (selectedChain) {
-      const selectedChainData = blockchains.find(chain => chain.name === selectedChain);
-      const sharedProtocolChains = blockchains.filter(chain => 
-        chain.protocol.some(protocol => selectedChainData.protocol.includes(protocol))
+    if (searchTerm) {
+      const filteredChains = blockchains.filter(chain =>
+        chain.name.toLowerCase().includes(searchTerm.toLowerCase())
       );
-      // Filter out the selectedChain from the matchingChains list
-      const filteredMatchingChains = sharedProtocolChains.filter(chain => chain.name !== selectedChain);
-      setMatchingChains(filteredMatchingChains);
+      setMatchingChains(filteredChains);
     } else {
       setMatchingChains([]);
     }
-  }, [selectedChain]);
+  }, [searchTerm]);
 
   return (
-    <div className='flex w-full py-4 space-x-16 transform-all'>
-        
-      <div className=''>
-        <h2 className={'mb-4 transform-all text-xl font-bold ' + (selectedChain ? 'hidden' : 'block')}>Can my app go cross-chain with xCall?</h2>           
+    <div className='flex flex-col items-start w-full py-4 transform-all'>
+      {/* Search Bar */}
+      <div className='pb-8'>
+        <h2 className='mb-4 text-xl font-bold'>Can my app go cross-chain with xCall?</h2>
         <div className='flex space-x-8'>
-          {/* First Column: Dropdown for selecting a blockchain */}
           <div className='flex items-center'>
             <p className='mr-2'>My app is on</p>
-            <select className='px-2 py-1 rounded-full' onChange={(e) => setSelectedChain(e.target.value)} value={selectedChain}>
-              <option value="">Select a Blockchain</option>
-              {blockchains.map((chain, index) => (
-                <option className='flex items-center' key={index} value={chain.name}>
-                  {chain.name}
-                </option>
-              ))}
-            </select>
-          </div>            
-        </div>  
+            <input 
+              type="text" 
+              className='px-2 py-1 rounded-full' 
+              placeholder="Search your Blockchain"
+              onChange={(e) => setSearchTerm(e.target.value)} 
+              value={searchTerm} 
+            />
+          </div>
+        </div>
       </div>
-        
-        <div className={'p-4 bg-green-200/20 backdrop-blur rounded-xl transform-all ' + (selectedChain ? 'block' : 'hidden')}>         
-            <p className={`font-bold mb-4`}>Use xCall to seamlessly connect your app with</p>
-            <div className='flex items-center justify-around'>
-                <img className='w-8 h-8 mr-2' src={`/chainlogos/${selectedChain}.svg`} alt={selectedChain} />
-                        
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
-                </svg>
 
-                <div className='flex flex-col' >
-                    {matchingChains.map((chain, index) => (
-                    <div key={index}>{chain.name}</div>
-                    ))}
-                </div>
-            </div>           
+      {/* Matching Chains Display */}
+      <div className={`py-4 bg-green-200/${bgopacity} backdrop-blur rounded-xl text-center space-y-8 transform-all ` + (searchTerm ? 'block ' : 'hidden ')}>
+        <p className={'mx-8 mb-4 font-bold ' + (matchingChains.length > 0 ? 'inline-block' : 'hidden')}>xCall is compatible with</p>
+        <div className={matchingChains.length > 0 ? 'hidden' : 'inline-block'}>
+          <p className={'font-bold mb-4' }>xCall currently has no compatibility with <span className='italic'>"{searchTerm}"</span></p>
+          <button className={'px-4 py-2 font-medium bg-xteal/60 rounded-full block '} href="https://icon.community/integrations/">View xCall Integration Overview</button>
         </div>
         
+        <div className='flex items-center justify-around mx-8 space-x-8 '>
+          {matchingChains.map((chain, index) => (
+            <div key={index} className='flex flex-col items-center'>
+              <img className='w-8 h-8 mb-2' src={chain.logo} alt={chain.name} />
+              <div className='font-medium'>{chain.name}</div>
+            </div>
+          ))}
+        </div>
+        <a href="/getting-started" className={'mx-8 px-4 py-2 font-medium  bg-black rounded-full ' + (matchingChains.length > 0 ? 'inline-block' : 'hidden')}>Get Started</a>
+      </div>
+      
     </div>
   );
 };
 
 export default ChainChecker;
+
