842fe3c743bfa16937529c94458e67c55b3d0bb7
David <98425402+DavidFBD@users.noreply.github.com>
2024-01-22 15:13:14 +0100

    Fez check

--- components/chainchecker.js
+++ components/chainchecker.js
@@ -19,11 +19,11 @@ const ChainChecker = () => {
   }, [searchTerm]);
 
   return (
-    <div className='flex flex-col items-start w-full py-4 transform-all'>
+    <div className='flex flex-col items-center w-full py-0 sm:py-4 sm:items-start transform-all'>
       {/* Search Bar */}
       <div className='pb-8'>
-        <h2 className='mb-4 text-xl font-bold'>Can my app go cross-chain with xCall?</h2>
-        <div className='flex space-x-8'>
+        <h2 className='hidden mb-4 text-xl font-bold sm:block'>Can my app go cross-chain with xCall?</h2>
+        <div className='flex justify-center sm:justify-start'>
           <div className='flex items-center'>
             <p className='mr-2'>My app is on</p>
             <input 
@@ -38,22 +38,22 @@ const ChainChecker = () => {
       </div>
 
       {/* Matching Chains Display */}
-      <div className={`py-4 bg-green-200/${bgopacity} backdrop-blur rounded-xl text-center space-y-8 transform-all ` + (searchTerm ? 'block ' : 'hidden ')}>
+      <div className={`py-4 bg-green-200/${bgopacity} backdrop-blur rounded-xl text-center transform-all ` + (searchTerm ? 'block ' : 'hidden ')}>
         <p className={'mx-8 mb-4 font-bold ' + (matchingChains.length > 0 ? 'inline-block' : 'hidden')}>xCall is compatible with</p>
         <div className={matchingChains.length > 0 ? 'hidden' : 'inline-block'}>
           <p className={'font-bold mb-4' }>xCall currently has no compatibility with <span className='italic'>"{searchTerm}"</span></p>
-          <button className={'px-4 py-2 font-medium bg-xteal/60 rounded-full block '} href="https://icon.community/integrations/">View xCall Integration Overview</button>
+          <a className={'px-4 py-1.5 mx-auto font-medium bg-xteal/60 rounded-full block '} href="https://icon.community/integrations/">View xCall Integrations Overview</a>
         </div>
         
-        <div className='grid grid-cols-2 mx-8 sm:grid-cols-5'>
+        <div className={'grid mx-8 sm:flex justify-center ' + (matchingChains.length === 1 ? 'grid-cols-1' : 'grid-cols-2')} >
           {matchingChains.map((chain, index) => (
-            <div key={index} className='flex flex-col items-center mt-4 sm:mt-0'>
+            <div key={index} className={'flex flex-col items-center mx-4 mt-4 ' + (chain.status === 'mainnet' ? '' : 'grayscale opacity-30')}>
               <img className='w-8 h-8 mb-2' src={chain.logo} alt={chain.name} />
               <div className='font-medium'>{chain.name}</div>
             </div>
           ))}
         </div>
-        <a href="/getting-started" className={'mx-8 px-4 py-2 font-medium  bg-black rounded-full ' + (matchingChains.length > 0 ? 'inline-block' : 'hidden')}>Get Started</a>
+        <a href="https://icon.community/integrations/" target="_blank" className={'mx-8 mt-8 px-12 text-sm py-1.5 uppercase whitespace-nowrap font-medium  bg-xteal/60 rounded-full ' + (matchingChains.length > 0 ? 'inline-block ' : 'hidden')}>Integrations Overview ↗</a>
       </div>
       
     </div>
